1c9a252a03a42d1413fd999e9230fe3453b7d741
mfaisal <mfaisal@pnec.nust.edu.pk>
2021-09-27 11:47:06 +0500

    Changes added with react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,68 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import CardList from '../components/CardList';
 import SearchBox from "../components/SearchBox";
 import './App.css';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 
-class App extends React.Component{
 
-    constructor(){
+function App(){
 
-        super();
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchField] = useState('');
 
-        this.state = {
-            robots : [],
-            searchfield : ''
-        };
+    useEffect(()=>{
+            fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users=> {setRobots(users)});
+    }, []);
+
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value);
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
-    }
-
-    render(){
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot=>{
+    const filteredRobots = robots.filter(robot=>{
             return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         });
 
-        return !robots.length ?
+    return !robots.length ?
             <h1>Loading...</h1> :
         
              (
                 <div className="tc">
                     <h1 className="f2">Robofriends</h1>
-                    <SearchBox searchChange = {this.onSearchChange}/>
+                    <SearchBox searchChange = {onSearchChange}/>
                     <Scroll>
                         <ErrorBoundary>
                             <CardList robots={filteredRobots}/>
                         </ErrorBoundary>
                     </Scroll>
                 </div>
-            );
-        
-    }
-
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=> response.json())
-        .then(users=> this.setState({ robots: users}));
-        
-    }
-}
-
-
-
-// const App = () => {
-//     return (
-//         <div className="tc">
-//             <h1>Robofriends</h1>
-//             <SearchBox />
-//             <CardList robots={robots}/>
-//         </div>
-        
-//     );
-// }
+    );
+ }
 
 export default App;
