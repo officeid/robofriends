a6fabe4cbfa2fd686ddb0efe8e2ddc6bb332bbfc
mfaisal <mfaisal@pnec.nust.edu.pk>
2021-09-30 10:24:37 +0500

    Redux added

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,33 +1,94 @@
-import React, { useState, useEffect }  from 'react';
+// import React, { useState, useEffect }  from 'react';
+// import CardList from '../components/CardList';
+// import SearchBox from "../components/SearchBox";
+// import './App.css';
+// import Scroll from '../components/Scroll';
+// import ErrorBoundary from '../components/ErrorBoundary';
+
+
+// function App(){
+
+//     const [robots, setRobots] = useState([]);
+//     const [searchfield, setSearchField] = useState('');
+
+//     useEffect(()=>{
+//             // console.log(this.props.store);
+//             fetch('https://jsonplaceholder.typicode.com/users')
+//             .then(response=> response.json())
+//             .then(users=> {setRobots(users)});
+//     }, []);
+
+//     const onSearchChange = (event) => {
+//         setSearchField(event.target.value);
+//     }
+
+//     const filteredRobots = robots.filter(robot=>{
+//             return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+//         });
+
+//     return !robots.length ?
+//             <h1>Loading...</h1> :
+        
+//              (
+//                 <div className="tc">
+//                     <h1 className="f2">Robofriends</h1>
+//                     <SearchBox searchChange = {onSearchChange}/>
+//                     <Scroll>
+//                         <ErrorBoundary>
+//                             <CardList robots={filteredRobots}/>
+//                         </ErrorBoundary>
+//                     </Scroll>
+//                 </div>
+//     );
+//  } 
+
+// export default App;
+
+// ------------------------------------------------------------------------------
+/* App.js without React Hooks */
+
+import React from 'react';
 import CardList from '../components/CardList';
 import SearchBox from "../components/SearchBox";
 import './App.css';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
+import { setSearchField, requestRobots} from '../actions';
+import {connect} from "react-redux";
+// import { requestRobots } from '../reducers';
 
 
-function App(){
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
 
-    const [robots, setRobots] = useState([]);
-    const [searchfield, setSearchField] = useState('');
+const mapDispatchToProps = (dispatch) => {
+   return {
+     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+   }
+}
 
-    useEffect(()=>{
-            fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response=> response.json())
-            .then(users=> {setRobots(users)});
-    }, []);
+class App extends React.Component{
 
-    const onSearchChange = (event) => {
-        setSearchField(event.target.value);
+    componentDidMount(){
+        this.props.onRequestRobots();
     }
 
-    const filteredRobots = robots.filter(robot=>{
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    render(){
+        const { searchField, onSearchChange, robots, isPending} =this.props;
+        const filteredRobots = robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
         });
 
-    return !robots.length ?
+        return isPending ?
             <h1>Loading...</h1> :
-        
+
              (
                 <div className="tc">
                     <h1 className="f2">Robofriends</h1>
@@ -38,7 +99,8 @@ function App(){
                         </ErrorBoundary>
                     </Scroll>
                 </div>
-    );
- }
+            );
+    }
+}
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App); 
